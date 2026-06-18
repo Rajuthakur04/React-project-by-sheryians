b13a0c6d0316135a5d9094b843e2b343c4286bc6
Rajuthakur04 <thakurraju3540@gmail.com>
2026-06-18 23:59:48 +0530

    Mac-os clone

--- MAC-OS/src/Component/windows/MacWindow.jsx
+++ MAC-OS/src/Component/windows/MacWindow.jsx
@@ -4,8 +4,11 @@ const MacWindow = () => {
   return (
     <div>
       <Rnd>
-        <img src="https://images.unsplash.com/photo-1717632254835-6343cafd13f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1hYyUyMG9zJTIwdGVybWluYWx8ZW58MHx8MHx8fDA%3D" height={400} width={400} alt="" />
-      </Rnd>
+        <div className="window">
+          <div className="nav"></div>
+          <div className="main-content"></div>
+        </div>
+       </Rnd>
     </div>
   )
 }
